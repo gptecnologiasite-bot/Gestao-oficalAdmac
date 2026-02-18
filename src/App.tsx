693c4941f88a68793754643e12ec6c3f8921b76d
humberto <gptecnologiasite@gmail.com>
2026-02-18 18:11:30 -0300

    nao quebrar paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { FinanceProvider } from './context/FinanceContext'
 import { ThemeProvider } from './context/ThemeContext'
